8d79bb1e5fc671242f30612c268f56727c9c7c46
Rajeev Soni <rajeev.soni@onsolve.com>
2019-11-30 01:15:54 +0530

    Initial commit

---
+++ FirebaseDynamicLink.Net/LinkGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirebaseDynamicLink.Net
+{
+    public class LinkGenerator
+    {
+        public string Link { get; set; }
+
+        public string GetDynamicLink()
+        {
+            Console.WriteLine("initial commit");
+            return "http://localhost:8080";
+        }
+    }
+}
